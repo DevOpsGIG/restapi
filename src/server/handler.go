8893d3955c7f52eaba0979c353118bcc26ef0ce4
Alessandro Resta <alessandro.resta@gmail.com>
2017-01-04 19:48:51 +0000

    Resolve arith task flow
    Redirect task from producer to subscriber and send result back to the caller.
    
    Some polishing.
    
    Closes: #1

--- src/server/handler.go
+++ src/server/handler.go
@@ -1,14 +1,21 @@
 package server
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+
+	"io/ioutil"
 
 	"github.com/gorilla/mux"
 )
 
-// Handlers ...
+// ArithURL for the subscriber service
+const ArithURL = "http://127.0.0.1:8001/arithsubscriber"
+
+// Handlers its all here...
 func handlers() *mux.Router {
 	r := mux.NewRouter()
 	r.HandleFunc("/ping", handlerPing)
@@ -16,15 +23,50 @@ func handlers() *mux.Router {
 	return r
 }
 
+// handles arithmetic requests
 func handlerArith(w http.ResponseWriter, r *http.Request) {
+
+	// Parses the raw query from the POST form received
 	if err := r.ParseForm(); err != nil {
-		log.Print(err)
+		log.Print("rest api: failed to parse form - ", err)
 	}
-	for _, v := range r.Form {
-		fmt.Println(v[0])
+
+	// Grab the task and wait for a result
+	result := arithSender(r.PostFormValue("task"))
+
+	// Read the result
+	resultData, err := ioutil.ReadAll(result)
+	if err != nil {
+		log.Fatal("rest api: failed to read result - ", err)
 	}
+	defer result.Close()
+
+	// Send it back to the producer
+	w.Header().Set("X-Custom-Header", "arithsubcriber result")
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(resultData)
 }
 
+// just a handler for testing
 func handlerPing(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "pong")
+}
+
+// Send the task to arithsubcriber
+func arithSender(task string) io.ReadCloser {
+
+	// Make a POST reques to the subscriber with the task
+	req, err := http.NewRequest("POST", ArithURL, bytes.NewBuffer([]byte(task)))
+
+	// Set header
+	req.Header.Set("X-Custom-Header", "arithproducer task")
+	req.Header.Set("Content-Type", "application/json")
+
+	// Send the request
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return resp.Body
 }
